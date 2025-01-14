40ba4f3f0154d15c8fdb30ee64d883ba943e1952
luanfs <luanfonseca93@gmail.com>
2025-01-14 10:03:45 -0500

    hydro compilation file

---
+++ scripts/compilation/compile_hydro.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script to compile the non hydrostatic solver
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./compile.sh COMP clean"
+  echo "choices for COMP :  debug, repro, prod"
+  echo "choices for clean:  noclean, clean, cleanall"
+  exit 1
+fi
+
+COMP="$1"
+clean="$2"
+compiler="intel"
+
+model=hydro
+cd ../../SHiELD_build/Build
+./COMPILE solo $model $COMP $compiler 64bit $clean
+
+# Copy the log file to test directory
+cp -r build_solo_$model.$COMP.64bit.$compiler.out ../../scripts/compilation/build_solo_$model.$COMP.64bit.$compiler.out
